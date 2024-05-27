0168092c22a64ee6de206fe052a442148a9bf4e1
ColePal <colepalffy1@gmail.com>
2024-05-27 12:16:41 +1200

    Update map.py

--- map.py
+++ map.py
@@ -5,8 +5,21 @@ import leafmap
 #filepath = 'C:\Users\colep\OneDrive - Massey University\Data Wrangling and Machine Learning\Assignment 3 Group Project Personal'
 
 Area_Coords = pd.read_csv(r"Geo_info.csv")
+print(Area_Coords.columns)
 #Area_Coords['info'] = 'hello'
-
+Area_Coords[['Respondents','Area','Latitude', 'Longitude', '15-29 years',
+       '30-64 years', '65 years and over', 'No qualification',
+       'Level 3 certificate', 'Level 4 certificate', 'Level 5 diploma', 'Level 6 diploma',
+       'Bachelor degree and level 7 qualification',
+       'Post-graduate and honours degrees', 'Masters degree',
+       'Doctorate degree',
+       'European', 'Maori',
+       'Pacific Peoples', 'Asian', 'Middle Eastern/Latin American/African',
+       'Other ethnicity', 'Regular smoker', 'Ex-smoker',
+       'Never smoked regularly', 'No dependent children',
+       'One dependent child', 'Two dependent children',
+       'Three dependent children', 'Four or more dependent children',
+       'Total people - with at least one religious affiliation', 'No religion']]
 import streamlit as st
 import leafmap.foliumap as leafmap
 
@@ -18,7 +31,6 @@ with st.expander("See source code"):
     with st.echo():
 
         m = leafmap.Map(center=[-36.8, 175], zoom=8.5)
-        cities = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv'
         regions = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson'
 
        	#m.add_geojson(regions, layer_name='NZ Electorates')
